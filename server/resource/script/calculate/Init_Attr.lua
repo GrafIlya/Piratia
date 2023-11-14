c28e2d5b691a2afb54c9725db2fd401b71c74960
Graf <ilyaantar@yandex.ru>
2023-11-14 17:06:15 +0300

    Редактирование максимальных характеристик и комментарии

--- server/resource/script/calculate/Init_Attr.lua
+++ server/resource/script/calculate/Init_Attr.lua
@@ -2,53 +2,53 @@ print( "Loading Init_Attr.lua" )
 
 function Init_attr() 
 --LG("init_attr", "enter function initattr : " , "\n" ) 
-for i = 0, ATTR_MAX_NUM, 1 do							--іхКј»ЇЛщУРКфРФ
-	SetChaAttrMax( i, 100000000) 
-end 
+	for i = 0, ATTR_MAX_NUM, 1 do							
+		SetChaAttrMax( i, 100000000) 
+	end 
 end 
 
 Init_attr() 
 
-SetChaAttrMax( ATTR_LV		,	100			)	-- ЅЗЙ«µИј¶
-SetChaAttrMax( ATTR_HP		,	2000000000	)	-- µ±З°HP
-SetChaAttrMax( ATTR_SP		,	2000000000	)	-- µ±З°SP
-SetChaAttrMax( ATTR_JOB		,	100			)	-- ЅЗЙ«Ц°Тµ
-SetChaAttrMax( ATTR_CEXP		,	4200000000	)	-- µ±З°ѕ­Сй
-SetChaAttrMax( ATTR_NLEXP	,	4200000000	)	-- ПВТ»ј¶ЛщРиѕ­Сй
-SetChaAttrMax( ATTR_AP		,	300			)	-- КфРФµг
-SetChaAttrMax( ATTR_TP		,	200			)	-- јјДЬµг
-SetChaAttrMax( ATTR_GD		,	2000000000	)	-- ЅрЗ®
-SetChaAttrMax( ATTR_CLEXP	,	4200000000	)	--µ±З°µИј¶µДѕ­Сй
-SetChaAttrMax( ATTR_MXHP	,	2000000000	)	--Чоґуhp
-SetChaAttrMax( ATTR_MXSP		,	2000000000	)	--Чоґуsp
-SetChaAttrMax( ATTR_BSTR		,	100			)	-- »щ±ѕБ¦Бї             
-SetChaAttrMax( ATTR_BDEX		,	100			)	-- »щ±ѕБйЗЙ             
-SetChaAttrMax( ATTR_BAGI		,	100			)	-- »щ±ѕГфЅЭ             
-SetChaAttrMax( ATTR_BCON	,	100			)	-- »щ±ѕМеЦК             
-SetChaAttrMax( ATTR_BSTA		,	100			)	-- »щ±ѕѕ«Б¦             
-SetChaAttrMax( ATTR_BLUK		,	100			)	-- »щ±ѕРТФЛ             
-SetChaAttrMax( ATTR_BMXHP	,	2000000000	)	-- »щ±ѕЧоґуHP         
-SetChaAttrMax( ATTR_BMXSP	,	2000000000	)	-- »щ±ѕЧоґуSP         
-SetChaAttrMax( ATTR_BMNATK	,	9999			)	-- »щ±ѕЧоРЎ№Ґ»чБ¦    
-SetChaAttrMax( ATTR_BMXATK	,	9999			)	-- »щ±ѕЧоґу№Ґ»чБ¦    
-SetChaAttrMax( ATTR_BDEF		,	9999			)	-- »щ±ѕ·АУщБ¦          
-SetChaAttrMax( ATTR_BHIT		,	9999			)	-- »щ±ѕГьЦРВК          
-SetChaAttrMax( ATTR_BFLEE	,	9999			)	-- »щ±ѕЙБ±ЬВК          
-SetChaAttrMax( ATTR_BMF		,	9999			)	-- »щ±ѕС°±¦ВК          
-SetChaAttrMax( ATTR_BCRT		,	9999			)	-- »щ±ѕ±¬»чВК          
-SetChaAttrMax( ATTR_BHREC	,	9999			)	-- »щ±ѕhp»ЦёґЛЩ¶И   
-SetChaAttrMax( ATTR_BSREC	,	9999			)	-- »щ±ѕsp»ЦёґЛЩ¶И    
-SetChaAttrMax( ATTR_BASPD	,	9999			)	-- »щ±ѕ№Ґ»чјдёф       
-SetChaAttrMax( ATTR_BADIS	,	9999			)	-- »щ±ѕ№Ґ»чѕаАл       
-SetChaAttrMax( ATTR_BMSPD	,	9999			)	-- »щ±ѕТЖ¶ЇЛЩ¶И       
-SetChaAttrMax( ATTR_BCOL		,	9999			)	-- »щ±ѕЧКФґІЙјЇЛЩ¶И 
-SetChaAttrMax( ATTR_MSPD	,	9999			)	--ТЖ¶ЇЛЩ¶И
-SetChaAttrMax( ATTR_LHAND_ITEMV	,	9999			)	--ЧуКЦµАѕЯјУіЙ
-SetChaAttrMax( ATTR_BOAT_SHIP	,	100000			)	--ґ¬Ц»ID
-SetChaAttrMax( ATTR_BOAT_HEADER	,	100000			)	--ґ¬Н·АаРН
-SetChaAttrMax( ATTR_BOAT_BODY	,	100000			)	--ґ¬ЙнАаРН
-SetChaAttrMax( ATTR_BOAT_ENGINE	,	100000			)	--ґ¬ТэЗжАаРН
-SetChaAttrMax( ATTR_BOAT_CANNON	,	100000			)	--ґ¬»рЕЪАаРН
-SetChaAttrMax( ATTR_BOAT_PART	,	100000			)	-- ґ¬ЕдјюАаРН
-SetChaAttrMax( ATTR_BOAT_BERTH	,	100000			)	--ґ¬Ц»НЈІґёЫїЪ
-SetChaAttrMax( ATTR_BOAT_DBID	,	2000000000			)	--ґ¬ЦчµДID
+SetChaAttrMax( ATTR_LV			,	100				)	-- Максимальный уровень
+SetChaAttrMax( ATTR_HP			,	2000000000		)	-- Максимальный HP
+SetChaAttrMax( ATTR_SP			,	2000000000		)	-- Максимальный SP
+SetChaAttrMax( ATTR_JOB			,	100				)	-- Максимальное кол-во профессий
+SetChaAttrMax( ATTR_CEXP		,	4200000000		)	-- 
+SetChaAttrMax( ATTR_NLEXP		,	4200000000		)	-- 
+SetChaAttrMax( ATTR_AP			,	300				)	-- 
+SetChaAttrMax( ATTR_TP			,	200				)	-- 
+SetChaAttrMax( ATTR_GD			,	2000000000		)	-- Максимальное кол-во золота
+SetChaAttrMax( ATTR_CLEXP		,	4200000000		)	--
+SetChaAttrMax( ATTR_MXHP		,	2000000000		)	--
+SetChaAttrMax( ATTR_MXSP		,	2000000000		)	--
+SetChaAttrMax( ATTR_BSTR		,	100				)	-- Максимальное кол-во силы         
+SetChaAttrMax( ATTR_BDEX		,	100				)	-- Максимальное кол-во точности             
+SetChaAttrMax( ATTR_BAGI		,	100				)	-- Максимальное кол-во ловкости             
+SetChaAttrMax( ATTR_BCON		,	100				)	-- Максимальное кол-во телосложения             
+SetChaAttrMax( ATTR_BSTA		,	100				)	-- Максимальное кол-во духа             
+SetChaAttrMax( ATTR_BLUK		,	100				)	-- Максимальное кол-во удачи             
+SetChaAttrMax( ATTR_BMXHP		,	2000000000		)	-- 
+SetChaAttrMax( ATTR_BMXSP		,	2000000000		)	-- 
+SetChaAttrMax( ATTR_BMNATK		,	9999			)	-- 
+SetChaAttrMax( ATTR_BMXATK		,	9999			)	-- 
+SetChaAttrMax( ATTR_BDEF		,	9999			)	-- 
+SetChaAttrMax( ATTR_BHIT		,	9999			)	-- 
+SetChaAttrMax( ATTR_BFLEE		,	9999			)	-- 
+SetChaAttrMax( ATTR_BMF			,	9999			)	-- 
+SetChaAttrMax( ATTR_BCRT		,	9999			)	-- 
+SetChaAttrMax( ATTR_BHREC		,	9999			)	-- 
+SetChaAttrMax( ATTR_BSREC		,	9999			)	-- 
+SetChaAttrMax( ATTR_BASPD		,	9999			)	-- 
+SetChaAttrMax( ATTR_BADIS		,	9999			)	-- 
+SetChaAttrMax( ATTR_BMSPD		,	9999			)	-- 
+SetChaAttrMax( ATTR_BCOL		,	9999			)	-- 
+SetChaAttrMax( ATTR_MSPD		,	9999			)	--
+SetChaAttrMax( ATTR_LHAND_ITEMV	,	9999			)	--
+SetChaAttrMax( ATTR_BOAT_SHIP	,	100000			)	--
+SetChaAttrMax( ATTR_BOAT_HEADER	,	100000			)	--
+SetChaAttrMax( ATTR_BOAT_BODY	,	100000			)	--
+SetChaAttrMax( ATTR_BOAT_ENGINE	,	100000			)	--
+SetChaAttrMax( ATTR_BOAT_CANNON	,	100000			)	--
+SetChaAttrMax( ATTR_BOAT_PART	,	100000			)	--
+SetChaAttrMax( ATTR_BOAT_BERTH	,	100000			)	--
+SetChaAttrMax( ATTR_BOAT_DBID	,	2000000000		)	--
